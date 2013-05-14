1dffe5314db159d315b08d4bb55b62e91c548ed2
C Rice <colm_rice@hotmail.com>
2013-05-14 16:26:07 +0100

    Updated with M. Johnson libsvm

--- MLC/Form1.Designer.cs
+++ MLC/Form1.Designer.cs
@@ -30,15 +30,17 @@
         {
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
             this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.button1 = new System.Windows.Forms.Button();
             this.tabPage2 = new System.Windows.Forms.TabPage();
-            this.checkBox2 = new System.Windows.Forms.CheckBox();
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
             this.groupBox1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
             this.SuspendLayout();
             // 
             // tabControl1
@@ -64,6 +66,14 @@
             this.tabPage1.Text = "Converter";
             this.tabPage1.UseVisualStyleBackColor = true;
             // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(7, 142);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(200, 96);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
             // groupBox1
             // 
             this.groupBox1.Controls.Add(this.checkBox2);
@@ -74,6 +84,16 @@
             this.groupBox1.TabIndex = 1;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "From/To Formats";
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(7, 44);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(112, 17);
+            this.checkBox2.TabIndex = 1;
+            this.checkBox2.Text = "Some other format";
+            this.checkBox2.UseVisualStyleBackColor = true;
             // 
             // checkBox1
             // 
@@ -99,31 +119,24 @@
             // 
             // tabPage2
             // 
+            this.tabPage2.Controls.Add(this.button2);
             this.tabPage2.Location = new System.Drawing.Point(4, 22);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
             this.tabPage2.Size = new System.Drawing.Size(640, 406);
             this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.Text = "M. Johnson - libsvm 1.6.3";
             this.tabPage2.UseVisualStyleBackColor = true;
             // 
-            // checkBox2
+            // button2
             // 
-            this.checkBox2.AutoSize = true;
-            this.checkBox2.Location = new System.Drawing.Point(7, 44);
-            this.checkBox2.Name = "checkBox2";
-            this.checkBox2.Size = new System.Drawing.Size(112, 17);
-            this.checkBox2.TabIndex = 1;
-            this.checkBox2.Text = "Some other format";
-            this.checkBox2.UseVisualStyleBackColor = true;
-            // 
-            // richTextBox1
-            // 
-            this.richTextBox1.Location = new System.Drawing.Point(7, 142);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(200, 96);
-            this.richTextBox1.TabIndex = 2;
-            this.richTextBox1.Text = "";
+            this.button2.Location = new System.Drawing.Point(7, 7);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(180, 23);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Run SVM";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form1
             // 
@@ -137,6 +150,7 @@
             this.tabPage1.ResumeLayout(false);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -151,6 +165,7 @@
         private System.Windows.Forms.TabPage tabPage2;
         private System.Windows.Forms.CheckBox checkBox2;
         private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- MLC/Form1.cs
+++ MLC/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 using System.IO;
+using SVM;
 
 namespace MLC
 {
@@ -41,6 +42,40 @@ namespace MLC
         private void checkBox1_CheckedChanged(object sender, EventArgs e)
         {
             button1.Enabled = true;
+        }
+
+
+        //SVM - M. Johnson
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Problem train = Problem.Read(@"C:\\libsvm\\diabetes.train");
+            Problem test = Problem.Read(@"C:\\libsvm\\diabetes.test");
+
+            //For this example (and indeed, many scenarios), the default
+            //parameters will suffice.
+            Parameter parameters = new Parameter();
+            double C;
+            double Gamma;
+
+
+            //This will do a grid optimization to find the best parameters
+            //and store them in C and Gamma, outputting the entire
+            //search to params.txt.
+
+            ParameterSelection.Grid(train, parameters, @"C:\\libsvm\\params.txt", out C, out Gamma);
+            parameters.C = C;
+            parameters.Gamma = Gamma;
+
+
+            //Train the model using the optimal parameters.
+
+            Model model = Training.Train(train, parameters);
+
+
+            //Perform classification on the test data, putting the
+            //results in results.txt.
+
+            Prediction.Predict(test, @"C:\\libsvm\\results.txt", model, false);
         }
 
 
